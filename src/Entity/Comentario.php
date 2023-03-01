acbb69ce7972d05e68199e95c7353d6d84ff0477
Saúl Hernández Arellano <shernandeza@correo.uam.mx>
2023-03-01 11:49:31 -0600

    Se crea la entidada Comentario

---
+++ src/Entity/Comentario.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ComentarioRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ComentarioRepository::class)]
+class Comentario
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $texto = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTexto(): ?string
+    {
+        return $this->texto;
+    }
+
+    public function setTexto(string $texto): self
+    {
+        $this->texto = $texto;
+
+        return $this;
+    }
+}
